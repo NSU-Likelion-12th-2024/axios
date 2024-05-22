0a1d66c746be5d67c4c8e6068329331a1bc1fee9
서병덕 <qudejr0000@naver.com>
2024-05-22 16:13:53 +0900

    수정

--- src/inquiry/inquiry.js
+++ src/inquiry/inquiry.js
@@ -28,10 +28,8 @@ function Inquiry() {
     event.preventDefault();
      try {
             // axios를 사용하여 POST 요청을 보냅니다.
-            const response = await axios.post(`${awsIP}/form/submit`, { 
-              user: email,           // 요청의 본문에 포함될 데이터: 사용자 이메일
-              userName: name,        // 요청의 본문에 포함될 데이터: 사용자 이름
-              userDetail: content    // 요청의 본문에 포함될 데이터: 사용자 질문 내용
+            const response = await  { 
+              user: email, 
             }, {
               headers: {
                 'Content-Type': 'application/json'  // 요청 헤더 설정: JSON 형식으로 데이터를 전송함을 명시
@@ -39,24 +37,20 @@ function Inquiry() {
             });
             
             // 서버로부터의 응답 상태를 확인합니다.
-            if (response.status === 200) {
+            if (response.status === ) {
               // 응답 상태가 200 (성공)인 경우
-              setSubmitMessage('질문이 성공적으로 전송되었습니다.');  // 성공 메시지 설정
             } else {
               // 응답 상태가 200이 아닌 경우
-              setSubmitMessage('질문 전송 실패했습니다. 다시 시도해주세요.');  // 실패 메시지 설정
             }
           } catch (error) {
             // 예외가 발생한 경우 (네트워크 오류, 서버 오류 등)
-            console.error('질문 전송 실패:', error);  // 오류 내용을 콘솔에 출력
-            setSubmitMessage('질문 전송 실패했습니다. 다시 시도해주세요.');  // 실패 메시지 설정
           }
           
           // 요청 후 입력 필드를 초기화합니다.
           setEmail('');   
           setName('');
           setContent('');
-        }    
+        }
         
       
       
